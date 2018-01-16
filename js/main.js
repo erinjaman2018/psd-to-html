2d50a3af225bb74fa0ff352b93dbfed07c9a367e
erinjaman <erinjaman14@gmail.com>
2018-01-16 12:34:29 +0600

    psd to html project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+<script>
+      var owl = $('.owl-carousel');
+      owl.owlCarousel({
+        margin: 10,
+        loop: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 2
+          },
+          1000: {
+            items: 3
+          }
+        }
+      })
+    </script>
